ba959c95a3f7352922c415b9d3eda765d18a0e67
Matheus Rodrigues <matheus.rodrigues@havan.com.br>
2023-12-14 16:45:50 -0300

    Add project files.

---
+++ RabbitMq.Sender/MessageSender.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMq.Sender
+{
+  public static class MessageSender
+  {
+    public static bool Send(string message)
+    {
+      try
+      {
+        var factory = new ConnectionFactory();
+
+        factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+        factory.ClientProvidedName = "Rabbit Sender Console";
+
+        var connection = factory.CreateConnection();
+        var channel = connection.CreateModel();
+
+        var exchangeName = "exchange";
+        var routingKey = "routing-key";
+        var queueName = "nome-fila";
+
+        channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+        channel.QueueDeclare(queueName, false, false, false, null);
+        channel.QueueBind(queueName, exchangeName, routingKey!, null);
+
+        var messageBodyBytes = Encoding.UTF8.GetBytes(message);
+        channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+
+        channel.Close();
+        connection.Close();
+
+        return true;
+      }
+      catch (Exception)
+      {
+        ///Gravar logs da exceção
+        return false;
+      }
+    }
+  }
+}
---
+++ RabbitMq.Sender/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace RabbitMq.Sender
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Escreva uma mensagem para enviar a fila");
+      var mensagem = Console.ReadLine();
+
+      var retorno = MessageSender.Send(mensagem);
+
+      if (!retorno)
+        Console.WriteLine("Ocorreu um erro ao enviar a mensagem");
+      else
+      {
+        Console.WriteLine("Mensagem enviada com sucesso, tecle para sair.");
+        Console.ReadLine();
+      }
+    }
+  }
+}
